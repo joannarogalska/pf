811aba41e91dfff10ed79a1be1061090cb3b9a28
Joanna Rogalska <joanna.rogalska@allegrogroup.com>
2017-07-30 22:24:53 +0200

    code refactor

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
-//import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
